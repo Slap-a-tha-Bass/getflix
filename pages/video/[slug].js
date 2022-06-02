2078144ba1b945f87e59d52bfebad651850749dd
Corey DeLoach <cdeloach16@gmail.com>
2022-06-02 15:41:19 -0500

    configuring env variables

--- pages/video/[slug].js
+++ pages/video/[slug].js
@@ -4,10 +4,10 @@ import styles from "../../styles/Home.module.css";
 
 export async function getStaticProps({ params }) {
   const client = new ApolloClient({
-    uri: process.env.NEXT_PRIVATE_API_ENDPOINT,
+    uri: `${process.env.NEXT_PRIVATE_API_ENDPOINT}`,
     cache: new InMemoryCache(),
     headers: {
-      authorization: process.env.NEXT_PRIVATE_GRAPH_CMS_TOKEN,
+      authorization: `Bearer ${process.env.NEXT_PRIVATE_GRAPH_CMS_TOKEN}`,
     },
   });
   const data = await client.query({
@@ -43,10 +43,10 @@ export async function getStaticProps({ params }) {
 }
 export async function getStaticPaths() {
   const client = new ApolloClient({
-    uri: process.env.NEXT_PRIVATE_API_ENDPOINT,
+    uri: `${process.env.NEXT_PRIVATE_API_ENDPOINT}`,
     cache: new InMemoryCache(),
     headers: {
-      authorization: process.env.NEXT_PRIVATE_GRAPH_CMS_TOKEN,
+      authorization: `Bearer ${process.env.NEXT_PRIVATE_GRAPH_CMS_TOKEN}`,
     },
   });
   const data = await client.query({
